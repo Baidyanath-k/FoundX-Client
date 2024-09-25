f174a4bc8a89e731cd11f7995a0a5ff7a5a58f02
Baidyanath-k <baidyanathkumar12091998@gmail.com>
2024-09-25 23:25:31 +0600

    FoundX setup

---
+++ src/app/found-items/page.tsx
@@ -0,0 +1,9 @@
+const FoundItems = () => {
+  return (
+    <div>
+      <h2>This is found Items</h2>
+    </div>
+  );
+};
+
+export default FoundItems;
---
+++ src/config/site.ts
@@ -0,0 +1,34 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "FoundX",
+  description: "If you are missing something, you will find its solution here!",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Found Items",
+      href: "/found-items",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Found Items",
+      href: "/found-items",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+};
